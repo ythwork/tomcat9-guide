74a5f60b432c32668b821421a59a0676360d35a7
ythwork <ythwork83@gmail.com>
2020-10-15 08:27:37 +0900

    tomcat9 guide

---
+++ bin/setenv.sh
@@ -0,0 +1,5 @@
+JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.http=9080"
+JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.https=9443"
+JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.ajp=9009"
+JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.shutdown=9005"
+
