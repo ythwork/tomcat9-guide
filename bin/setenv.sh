3ae852b22862cb12b9bb26318a938650fc843046
ythwork <ythwork83@gmail.com>
2020-10-16 11:36:55 +0900

    tomcat9-guide

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,3 +3,8 @@ JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.https=9443"
 JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.ajp=9009"
 JAVA_OPTS="$JAVA_OPTS -Dtomcat.port.shutdown=9005"
 
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=9002"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+
